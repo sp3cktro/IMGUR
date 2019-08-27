2e0ce49f4c393b6fb45be5d7ac4c60ad64abc820
Miguel Angel Gomez Rivero <miguelangel.gomez@mx-it00188.globant.com>
2019-08-27 10:59:24 -0500

    Initial commit

---
+++ IMGUR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IMGUR
+//
+//  Created by Miguel Angel Gomez Rivero on 8/27/19.
+//  Copyright © 2019 specktro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
