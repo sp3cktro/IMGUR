9f9953236d20bdb508839f960a78fb18a0fa4a15
Miguel Angel Gomez Rivero <miguelangel.gomez@mx-it00188.globant.com>
2019-08-27 10:59:24 -0500

    feat: Add all view elements

---
+++ IMGUR/SearchPopUpViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SearchPopUpViewController.swift
+//  IMGUR
+//
+//  Created by Jordy Xavier Pazaran Reyes on 8/29/19.
+//  Copyright © 2019 specktro. All rights reserved.
+//
+
+import UIKit
+
+class SearchPopUpViewController: UIViewController {
+
+    
+    //MARK: - Outlets
+    @IBOutlet weak var searchView: UIView!
+    @IBOutlet weak var keywordTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    //MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let searchContainer = searchView
+        searchContainer?.layer.cornerRadius = 6
+    
+        //Navigation controller configuration
+        navigationController?.isNavigationBarHidden = true
+        
+        let blurEffect = UIBlurEffect(style: .regular)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.frame
+        view.insertSubview(blurEffectView, at: 0)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    @IBAction func cancelAction(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func searchAction(_ sender: Any) {
+    }
+ 
+}
+
+
