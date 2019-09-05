316cf94732590548672e8069a820780542d09d60
sp3cktro <48292356+sp3cktro@users.noreply.github.com>
2019-09-05 01:05:51 -0500

    feat: Add first networking layer approach (#4)
    * chore: Add service layer to pixabay API
    * fix: Add security configuration to Info.plist
    * fix: Resolve project issues and rename folder to uppercase name
    * feat: Add parameter to getImages case
    * fix: Return decodable structs in ServiceLayer request.
    * fix: Add custom session to ServiceLayer request method

---
+++ IMGUR/network/Router.swift
@@ -0,0 +1,46 @@
+//
+//  Router.swift
+//  IMGUR
+//
+//  Created by specktro on 9/1/19.
+//  Copyright © 2019 specktro. All rights reserved.
+//
+
+import Foundation
+
+enum ImageRouter {
+    case getImages(String)
+    
+    var scheme: String {
+        return "https"
+    }
+    
+    var host: String {
+        switch self {
+        case .getImages:
+            return "pixabay.com"
+        }
+    }
+    
+    var path: String {
+        return "/api"
+    }
+    
+    var parameters: [URLQueryItem] {
+        let accessToken = "13435762-8cdfa77ad675450cea0066f89"
+        
+        switch self {
+        case .getImages(let queryString):
+            return [URLQueryItem(name: "key", value: accessToken),
+            URLQueryItem(name: "q", value: queryString),
+            URLQueryItem(name: "per_page", value: "10")]
+        }
+    }
+    
+    var method: String {
+        switch self {
+        case .getImages:
+            return "GET"
+        }
+    }
+}
---
+++ IMGUR/network/ServiceLayer.swift
@@ -0,0 +1,52 @@
+//
+//  ServiceLayer.swift
+//  IMGUR
+//
+//  Created by specktro on 9/3/19.
+//  Copyright © 2019 specktro. All rights reserved.
+//
+
+import Foundation
+
+class ServiceLayer {
+    class func request<T: Codable>(session: URLSession = .shared, router: ImageRouter, completion: @escaping (Result<T, Error>) -> ()) {
+        var components = URLComponents()
+        components.scheme = router.scheme
+        components.host = router.host
+        components.path = router.path
+        components.queryItems = router.parameters
+        
+        guard let url = components.url else {
+            return
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = router.method
+        let task = session.dataTask(with: urlRequest) { (data, response, error) in
+            guard error == nil else {
+                completion(.failure(error!))
+                return
+            }
+            
+            guard response != nil else {
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            
+            do {
+                let responseObject = try decoder.decode(T.self, from: data)
+                completion(.success(responseObject))
+            }
+            catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
+}
